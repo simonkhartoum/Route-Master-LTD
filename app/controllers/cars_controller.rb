02c91860c42dc3a9dca654319fb3ca4d07754b9c
erickariuki <eric.kariuki@student.moringaschool.com>
2023-07-04 15:27:34 +0300

    add routes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,24 @@
 class CarsController < ApplicationController
+    def index
+        cars = Car.all
+        render json: cars
+    end
+    def create
+    car = Car.create!(car_params)
+    render json: car, status: :created
+    end
+    def show 
+        car = find_car
+        render json:car, include: :owner
+    end
+
+    private
+
+    def find_car
+        Car.find(params[:id])
+    end
+
+    def car_params
+        params.permit(:number_plate, :image, :owner_id,:driver_name,:driver_contact)
+    end
 end
