646f29fab49f5813162e34bee785f1550570409d
simonkhartoum <simonkhartoum@gmail.com>
2023-07-04 15:59:48 +0300

    update completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cars, only: [:index, :show, :create]
+  resources :cars, only: [:index, :show, :create, :update]
   resources :owners, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
