5a5aa1a7c007d222b707109457f1f3e118657027
Chris <chris.kamau@student.moringaschool.com>
2023-07-06 09:31:58 +0300

    Completed Assignment

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage'
 import CarImage from './components/CarImage';
 import NavBar from './components/NavBar';
 import FormTable from './components/Admin';
+import Login from "./components/Login";
+
 function App  ()  {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
   return (
   <div>
   <BrowserRouter>
-  <NavBar />
+  <NavBar user={user} setUser={setUser} />
   <Routes>
-  <Route path="/" element={<HomePage />}></Route>
+  <Route path="/" element={<HomePage user={user} />}></Route>
   <Route path="/car" element={<CarImage />}></Route>
   <Route path="/form-table" element={<FormTable />}></Route>
   </Routes>
